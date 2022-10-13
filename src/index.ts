b21c0a7816355778a78516e6b7897cb672921c13
Eduardo Bravo <eduardojosebb.matescience@gmail.com>
2022-10-12 23:20:53 -0400

    typescript introduction

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+console.log("Hola desde typescript");
+console.log("Hola mundo, desde la ciudad de Merida");
+console.log("Merida, ciudad de los caballeros");
+
+// Esto es un comentario de una linea
+
+/**
+ * Esto es un comentario
+ * Multilinea
+ */
